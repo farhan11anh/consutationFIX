7599c73c4d1caa70f2f88fb332c285cef25168d4
Muhammad Riandi <62460140+riandiuniska@users.noreply.github.com>
2022-06-08 16:57:29 +0700

    Update validation di modal pada inputan waktu
    Update validation di modal pada inputan waktu

--- frontend/pages/index.php
+++ frontend/pages/index.php
@@ -364,7 +364,7 @@ for ($i = 0; $i < count($users); $i++) {
                                 <div>
                                     <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300 after:content-['*'] after:ml-0.5 after:text-red-500">Waktu</label>
                                     <div class="timepicker relative form-floating mb-3 w-full" id="input-toggle-timepicker" data-mdb-toggle-button="false">
-                                        <select name="time" id="time" class=" border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 -mb-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                        <select name="time" id="time" class=" border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 -mb-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required> 
                                         </select>
                                         <p class="mt-5 -mb-4 italic text-xs text-gray-500">
                                             Pilih waktu yang tersedia.
@@ -378,7 +378,7 @@ for ($i = 0; $i < count($users); $i++) {
                                     <!-- save lecture -->
                                     <input type="text" hidden name="lectureId" id="lecture" value="">
                                     <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300 after:content-['*'] after:ml-0.5 after:text-red-500">Topik</label>
-                                    <textarea id="message" rows="4" name="topic" class="block p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Tuliskan topik yang ingin dikonsultasikan..."></textarea>
+                                    <textarea id="message" rows="4" name="topic" class="block p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Tuliskan topik yang ingin dikonsultasikan..." required></textarea>
                                 </div>
                                 <div class="text-center">
                                     <button type="submit" name="submit" value="success" class="items-center focus:outline text-white border-yellow-400 bg-yellow-500 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">
